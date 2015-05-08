374a7bddc40ecdd809ee4e7e29f799cadad3b232
tunnckoCore <mameto_100@mail.bg>
2015-05-09 02:30:48 +0300

    reorganize tests

--- test.js
+++ test.js
@@ -27,96 +27,100 @@ test('detect-installed:', function () {
     test.throws(fixture, /expect `name` to be non empty string/)
     done()
   })
-  test('should checks globally asynchronous. (when exists)', function (done) {
-    detectInstalled('npm', function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, true)
-      done()
+  test('should checks globally', function () {
+    test('asynchronous, when exists', function (done) {
+      detectInstalled('npm', function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, true)
+        done()
+      })
+      detectInstalled('npm', false, function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, true)
+        done()
+      })
+      detectInstalled('npm', [1, 2, 3], function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, true)
+        done()
+      })
     })
-    detectInstalled('npm', false, function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, true)
-      done()
+    test('asynchronous, when not exists', function (done) {
+      detectInstalled('when not exists', function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, false)
+        done()
+      })
+      detectInstalled('when not exists', false, function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, false)
+        done()
+      })
+      detectInstalled('when not exists', [1, 2, 3], function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, false)
+        done()
+      })
     })
-    detectInstalled('npm', [1, 2, 3], function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, true)
-      done()
-    })
-  })
-  test('should checks globally asynchronous. (when not exists)', function (done) {
-    detectInstalled('when not exists', function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, false)
-      done()
-    })
-    detectInstalled('when not exists', false, function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, false)
-      done()
-    })
-    detectInstalled('when not exists', [1, 2, 3], function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, false)
-      done()
-    })
-  })
-  test('should checks globally synchronous. (when exists)', function (done) {
-    var actual1 = detectInstalled('npm')
-    var actual2 = detectInstalled('npm', false)
-    var actual3 = detectInstalled('npm', [1, 2, 3])
-    var actual4 = detectInstalled('npm', [1, 2, 3], {not: 'a function'})
-    var expected = true
+    test('synchronous, when exists', function (done) {
+      var actual1 = detectInstalled('npm')
+      var actual2 = detectInstalled('npm', false)
+      var actual3 = detectInstalled('npm', [1, 2, 3])
+      var actual4 = detectInstalled('npm', [1, 2, 3], {not: 'a function'})
+      var expected = true
 
-    test.equal(actual1, expected)
-    test.equal(actual2, expected)
-    test.equal(actual3, expected)
-    test.equal(actual4, expected)
-    done()
-  })
-  test('should checks globally synchronous. (when not exists)', function (done) {
-    var actual1 = detectInstalled('when not exists')
-    var actual2 = detectInstalled('when not exists', false)
-    var actual3 = detectInstalled('when not exists', [1, 2, 3])
-    var actual4 = detectInstalled('when not exists', [1, 2, 3], {not: 'a function'})
-    var expected = false
+      test.equal(actual1, expected)
+      test.equal(actual2, expected)
+      test.equal(actual3, expected)
+      test.equal(actual4, expected)
+      done()
+    })
+    test('synchronous, when not exists', function (done) {
+      var actual1 = detectInstalled('when not exists')
+      var actual2 = detectInstalled('when not exists', false)
+      var actual3 = detectInstalled('when not exists', [1, 2, 3])
+      var actual4 = detectInstalled('when not exists', [1, 2, 3], {not: 'a function'})
+      var expected = false
 
-    test.equal(actual1, expected)
-    test.equal(actual2, expected)
-    test.equal(actual3, expected)
-    test.equal(actual4, expected)
-    done()
-  })
-  test('should checks locally asynchronous. (when exists)', function (done) {
-    detectInstalled('debug', true, function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, true)
+      test.equal(actual1, expected)
+      test.equal(actual2, expected)
+      test.equal(actual3, expected)
+      test.equal(actual4, expected)
       done()
     })
   })
-  test('should checks locally asynchronous. (when not exists)', function (done) {
-    detectInstalled('when not exists', true, function (err, actual) {
-      test.equal(err, null)
-      test.equal(actual, false)
+  test('should checks locally', function () {
+    test('asynchronous, when exists', function (done) {
+      detectInstalled('debug', true, function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, true)
+        done()
+      })
+    })
+    test('asynchronous, when not exists', function (done) {
+      detectInstalled('when not exists', true, function (err, actual) {
+        test.equal(err, null)
+        test.equal(actual, false)
+        done()
+      })
+    })
+    test('synchronous, when exists', function (done) {
+      var actual1 = detectInstalled('debug', true)
+      var actual2 = detectInstalled('debug', true, {not: 'a function'})
+      var expected = true
+
+      test.equal(actual1, expected)
+      test.equal(actual2, expected)
       done()
     })
-  })
-  test('should checks locally synchronous. (when exists)', function (done) {
-    var actual1 = detectInstalled('debug', true)
-    var actual2 = detectInstalled('debug', true, {not: 'a function'})
-    var expected = true
+    test('synchronous, when not exists', function (done) {
+      var actual1 = detectInstalled('npm', true)
+      var actual2 = detectInstalled('npm', true, {not: 'a function'})
+      var expected = false
 
-    test.equal(actual1, expected)
-    test.equal(actual2, expected)
-    done()
-  })
-  test('should checks locally synchronous. (when not exists)', function (done) {
-    var actual1 = detectInstalled('npm', true)
-    var actual2 = detectInstalled('npm', true, {not: 'a function'})
-    var expected = false
-
-    test.equal(actual1, expected)
-    test.equal(actual2, expected)
-    done()
+      test.equal(actual1, expected)
+      test.equal(actual2, expected)
+      done()
+    })
   })
 })

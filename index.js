aaedf01e068ae115f43f602abca0a845721a01cd
tunnckoCore <mameto_100@mail.bg>
2016-12-11 03:44:02 +0200

    chore(update): boilerplate

--- index.js
+++ index.js
@@ -1,54 +1,12 @@
 /*!
  * detect-installed <https://github.com/tunnckoCore/detect-installed>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
-var modules = require('global-modules')
-var cwd = process.cwd()
+module.exports = function detectInstalled () {
 
-module.exports = function detectInstalled (name, local, callback) {
-  if (!isValidString(name)) {
-    throw new TypeError('detect-installed: expect `name` be string')
-  }
-  var fp = path.join(modules, name)
-  var nm = path.join(cwd, 'node_modules', name)
-
-  if (typeof local === 'function') {
-    callback = local
-  }
-  if (typeof local === 'boolean') {
-    fp = local ? nm : fp
-  }
-  if (typeof callback === 'function') {
-    return statAsync(fp, callback)
-  }
-
-  return tryStatSync(fp)
-}
-
-function isValidString (val) {
-  return typeof val === 'string' ? val.length > 0 : false
-}
-
-function statAsync (fp, callback) {
-  fs.stat(fp, function (err, res) {
-    if (err) {
-      return callback(null, false)
-    }
-    callback(null, res.isDirectory())
-  })
-}
-
-function tryStatSync (fp) {
-  try {
-    return fs.statSync(fp).isDirectory()
-  } catch (err) {
-    return false
-  }
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * detect-installed <https://github.com/tunnckoCore/detect-installed>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
@@ -9,112 +9,10 @@
 
 'use strict'
 
-var test = require('assertit')
-var detectInstalled = require('./index')
+const test = require('mukla')
+const detectInstalled = require('./index')
 
-test('detect-installed:', function () {
-  test('should throw TypeError if `name` is not a string', function (done) {
-    function fixture () {
-      detectInstalled([1, 2, 3])
-    }
-    test.throws(fixture, TypeError)
-    test.throws(fixture, /detect-installed: expect `name` be string/)
-    done()
-  })
-  test('should checks globally', function () {
-    test('asynchronous, when exists', function (done) {
-      detectInstalled('npm', function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, true)
-        done()
-      })
-      detectInstalled('npm', false, function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, true)
-        done()
-      })
-      detectInstalled('npm', [1, 2, 3], function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, true)
-        done()
-      })
-    })
-    test('asynchronous, when not exists', function (done) {
-      detectInstalled('when not exists', function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, false)
-        done()
-      })
-      detectInstalled('when not exists', false, function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, false)
-        done()
-      })
-      detectInstalled('when not exists', [1, 2, 3], function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, false)
-        done()
-      })
-    })
-    test('synchronous, when exists', function (done) {
-      var actual1 = detectInstalled('npm')
-      var actual2 = detectInstalled('npm', false)
-      var actual3 = detectInstalled('npm', [1, 2, 3])
-      var actual4 = detectInstalled('npm', [1, 2, 3], {not: 'a function'})
-      var expected = true
-
-      test.equal(actual1, expected)
-      test.equal(actual2, expected)
-      test.equal(actual3, expected)
-      test.equal(actual4, expected)
-      done()
-    })
-    test('synchronous, when not exists', function (done) {
-      var actual1 = detectInstalled('when not exists')
-      var actual2 = detectInstalled('when not exists', false)
-      var actual3 = detectInstalled('when not exists', [1, 2, 3])
-      var actual4 = detectInstalled('when not exists', [1, 2, 3], {not: 'a function'})
-      var expected = false
-
-      test.equal(actual1, expected)
-      test.equal(actual2, expected)
-      test.equal(actual3, expected)
-      test.equal(actual4, expected)
-      done()
-    })
-  })
-  test('should checks locally', function () {
-    test('asynchronous, when exists', function (done) {
-      detectInstalled('global-modules', true, function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, true)
-        done()
-      })
-    })
-    test('asynchronous, when not exists', function (done) {
-      detectInstalled('when not exists', true, function (err, actual) {
-        test.equal(err, null)
-        test.equal(actual, false)
-        done()
-      })
-    })
-    test('synchronous, when exists', function (done) {
-      var actual1 = detectInstalled('global-modules', true)
-      var actual2 = detectInstalled('global-modules', true, {not: 'a function'})
-      var expected = true
-
-      test.equal(actual1, expected)
-      test.equal(actual2, expected)
-      done()
-    })
-    test('synchronous, when not exists', function (done) {
-      var actual1 = detectInstalled('npm', true)
-      var actual2 = detectInstalled('npm', true, {not: 'a function'})
-      var expected = false
-
-      test.equal(actual1, expected)
-      test.equal(actual2, expected)
-      done()
-    })
-  })
+test('detect-installed', function (done) {
+  detectInstalled()
+  done()
 })

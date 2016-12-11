a9586e7c61230e6be0802b45be483c1f92dfaccd
tunnckoCore <mameto_100@mail.bg>
2016-12-11 04:03:00 +0200

    refactor(*): use ES2015, allow custom cwd to be passed through options
    BREAKING CHANGE: default export is async and returns a promise; allow custom cwd to be passed
    
    through opts.cwd; pass opts.local to check if package exists locally.

--- index.js
+++ index.js
@@ -7,6 +7,48 @@
 
 'use strict'
 
-module.exports = function detectInstalled () {
+const fs = require('fs')
+const path = require('path')
+const modules = require('global-modules')
 
+module.exports = function detectInstalled (name, opts) {
+  return new Promise((resolve, reject) => {
+    if (!isValidString(name)) {
+      const message = 'detect-installed: expect `name` to be string'
+      return reject(new TypeError(message))
+    }
+
+    fs.stat(defaults(name, opts), (err, stats) => {
+      if (err) return reject(err)
+      resolve(stats.isDirectory())
+    })
+  })
+}
+
+module.exports.sync = function detectInstalledSync (name, opts) {
+  if (!isValidString(name)) {
+    throw new TypeError('detect-installed: expect `name` to be string')
+  }
+  return tryStatSync(defaults(name, opts))
+}
+
+const isValidString = (val) => {
+  return typeof val === 'string' ? val.length > 0 : false
+}
+
+const defaults = (name, opts) => {
+  opts = opts && typeof opts === 'object' ? opts : {}
+  opts.cwd = typeof opts.cwd === 'string' ? opts.cwd : process.cwd()
+
+  return opts.local
+    ? path.join(opts.cwd, 'node_modules', name)
+    : path.join(modules, name)
+}
+
+const tryStatSync = (fp) => {
+  try {
+     return fs.statSync(fp).isDirectory()
+  } catch (err) {
+     return false
+  }
 }
